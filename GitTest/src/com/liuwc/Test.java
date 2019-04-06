2a44e53e611c4dfb1d340afa1cb05e18613caaa6
此人有点儿优秀 <595187679@qq.com>
2019-04-06 21:52:09 +0800

    第二次提交

--- GitTest/src/com/liuwc/Test.java
+++ GitTest/src/com/liuwc/Test.java
@@ -22,6 +22,7 @@ public class Test {
 
     public static void main(String[] args){
       System.out.println("git");
-      System.out.println("第二次提交");
+      System.out.println("第三次提交");
+      System.out.println("这里是主干");
     }
 }
