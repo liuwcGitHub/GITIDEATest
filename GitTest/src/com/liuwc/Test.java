c1740ef8590f837e2e1c4e3779446d1087e148d3
此人有点儿优秀 <595187679@qq.com>
2019-04-06 21:22:29 +0800

    第一次提交输出git

---
+++ GitTest/src/com/liuwc/Test.java
@@ -0,0 +1,26 @@
+/**
+ * Copyright (C), 2018-2019, 公司
+ * FileName: Test
+ * Author:   刘伟成
+ * Date:     2019/4/6 21:18
+ * Description:
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+package com.liuwc;
+
+/**
+ * 〈一句话功能简述〉<br> 
+ * 〈〉
+ *
+ * @author 刘伟成
+ * @create 2019/4/6
+ * @since 1.0.0
+ */
+public class Test {
+
+    public static void main(String[] args){
+      System.out.println("git");
+    }
+}
